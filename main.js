7528ae156ec1830a20f5884276ac3f39814a396f
Taemin Kim <taeminkimny@gmail.com>
2022-06-02 15:49:21 -0400

    Added event listener for user to upload files

--- main.js
+++ main.js
@@ -13,12 +13,11 @@ const APP = {
         APP.canvas.height = 600;
         APP.canvas.style.height = 600;
         APP.img = document.createElement('img');
-        // APP.img.src = APP.canvas.getAttribute('data-src');
-        // testing
-        APP.img.src = imageFile;
-      //once the image is loaded, add it to the canvas
+
+        //once the image is loaded, add it to the canvas
         APP.img.onload = (ev) => {
         APP.ctx.drawImage(APP.img, 0, 0);
+
         //call the context.getImageData method to get the array of [r,g,b,a] values
         let imgDataObj = APP.ctx.getImageData(
             0,
@@ -29,10 +28,10 @@ const APP = {
         APP.data = imgDataObj.data; //data prop is an array
         // console.log(APP.data.length, 900 * 600 * 4); //  has 2,160,000 elements
         APP.canvas.addEventListener('mousemove', APP.getPixel);
-        APP.canvas.addEventListener('click', APP.addBox);
-        
+        APP.canvas.addEventListener('click', APP.addBox);      
         };
     },
+
     getPixel(ev) {
       //as the mouse moves around the image
       // let canvas = ev.target;
@@ -46,7 +45,6 @@ const APP = {
         document.getElementById('pixelColor').style.backgroundColor = clr;
       //save the string to use elsewhere
         APP.pixel = clr;
-        
     },
 
     getPixelColor(cols, x, y) {
@@ -70,7 +68,7 @@ const APP = {
           pixel.className = 'box';
           pixel.setAttribute('data-label', 'Exact pixel');
           pixel.setAttribute('data-color', APP.pixel);
-          scheme1.setAttribute('id', 'scheme1');
+          pixel.setAttribute('id', 'exactPixel');
           
           let scheme1 = document.createElement('span');
           scheme1.className = 'box';
@@ -78,27 +76,30 @@ const APP = {
           scheme1.setAttribute('data-color', APP.scheme1);
           scheme1.setAttribute('id', 'scheme1');
 
-      
           let scheme2 = document.createElement('span');
           scheme2.className = 'box';
           scheme2.setAttribute('data-label', 'Scheme2');
           scheme2.setAttribute('data-color', schemeList[1]);
+          scheme2.setAttribute('id', 'scheme2');
           
           let scheme3 = document.createElement('span');
           scheme3.className = 'box';
           scheme3.setAttribute('data-label', 'Scheme3');
           scheme3.setAttribute('data-color', schemeList[2]);
+          scheme3.setAttribute('id', 'scheme3');
   
           let scheme4 = document.createElement('span');
           scheme4.className = 'box';
           scheme4.setAttribute('data-label', 'Scheme4');
           scheme4.setAttribute('data-color', schemeList[3]);
+          scheme4.setAttribute('id', 'scheme4');
   
           let scheme5 = document.createElement('span');
           scheme5.className = 'box';
           scheme5.setAttribute('data-label', 'Scheme5');
           scheme5.setAttribute('data-color', schemeList[4]);
-  
+          scheme5.setAttribute('id', 'scheme5')
+
           pixel.style.backgroundColor = APP.pixel;
           scheme1.style.backgroundColor = schemeList[0];
           scheme2.style.backgroundColor = schemeList[1];
@@ -106,26 +107,21 @@ const APP = {
           scheme4.style.backgroundColor = schemeList[3];
           scheme5.style.backgroundColor = schemeList[4];
 
-
-  
           colours.append(pixel);
           colours.append(pixel, scheme1);
           colours.append(pixel, scheme2);
           colours.append(pixel, scheme3);
           colours.append(pixel, scheme4);
           colours.append(pixel, scheme5);
-        }, 500)
-
-        },
-    };
+        }, 500);
+    },
+  };
     
     
 
 
 
-// Connect to API
-
-
+// When user clicks it, fetch data from API and send it to color extraction tool
 
 function getColorScheme(elm) {
     fetch(`https://www.thecolorapi.com/scheme?rgb=${elm}`)
@@ -141,10 +137,28 @@ function getColorScheme(elm) {
 };
 
 
+const imageInput = document.getElementById('image_input')
+let uploadedImage = "";
+const imgURLList = [];
+let imgURL;
 
 
-// testing
-const imageFile = "./sample2.jpg"
+// Add Listener for users to upload files
+imageInput.addEventListener('change', getImage)
+
+function getImage(e) {
+    const reader = new FileReader()
+    reader.addEventListener('load',()=>{
+        uploadedImage = reader.result;
+        document.getElementById('display_image').style.backgroundImage = `url(${uploadedImage})`
+    })
+
+    reader.readAsDataURL(e.target.files[0])
+    imgURLList.unshift(e.target.files[0].name);
+    imgURL = 'sample4.jpg';
+
+    APP.img.src = imgURLList[0];
+}
 
 
 document.addEventListener('DOMContentLoaded', APP.init);
